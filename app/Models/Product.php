c6a320d66c659e58855bd5e77b356ffab4a8538f
xing <xzr2873917821@163.com>
2019-11-14 15:50:33 +0800

    商品列表

---
+++ app/Models/Product.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Product extends Model
+{
+    protected $fillable=[
+        'title',
+        'description',
+        'image',
+        'on_sale',
+        'rating',
+        'sold_count',
+        'review_count',
+        'price'
+    ];
+
+    protected $casts=[
+        'on_sale'=>'boolean'
+    ];
+
+    public function skus()
+    {
+        return $this->hasMany(ProductSku::class);
+    }
+
+    public function getImageUrlAttribute()
+    {
+        if(Str::startsWith($this->attributes['image'],['http://','https://'])){
+            return $this->attributes['image'];
+        }
+
+        return \Storage::disk('admin')->url($this->attributes['image']);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes(['verify' => true]);
 
+Route::get('products', 'ProductsController@index')->name('products.index');
+Route::redirect('/', '/products')->name('root');
+
 Route::group(['middleware'=>['auth','verified']],function (){
     Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
     Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
