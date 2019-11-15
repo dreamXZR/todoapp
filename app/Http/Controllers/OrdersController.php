2d1116f116fd952d1c17545aa5c12e65d1c769e7
xing <xzr2873917821@163.com>
2019-11-15 11:47:07 +0800

    用户界面 - 订单

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -67,4 +67,22 @@ class OrdersController extends Controller
 
         return $order;
     }
+
+    public function index(Request $request)
+    {
+        $orders=Order::query()
+            ->with(['items.product','items.productSku'])
+            ->where('user_id',$request->user()->id)
+            ->orderBy('created_at','desc')
+            ->paginate();
+
+        return view('orders.index', ['orders' => $orders]);
+    }
+
+    public function show(Order $order,Request $request)
+    {
+        $this->authorize('own',$order);
+
+        return view('orders.show', ['order' => $order->load(['items.productSku', 'items.product'])]);
+    }
 }
