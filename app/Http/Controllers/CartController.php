6c54aa687f871a6269ca87d18a0b8541b300b57e
xing <xzr2873917821@163.com>
2019-11-15 13:50:33 +0800

    业务代码封装

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,33 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\AddCartRequest;
+use App\Services\CartService;
 use Illuminate\Http\Request;
-use App\Models\CartItem;
 use App\Models\ProductSku;
 
 class CartController extends Controller
 {
+    protected $cartService;
+
+    public function __construct(CartService $cartService)
+    {
+        $this->cartService=$cartService;
+    }
+
     public function add(AddCartRequest $request)
     {
-        $user   = $request->user();
-        $skuId  = $request->input('sku_id');
-        $amount = $request->input('amount');
-
-        // 从数据库中查询该商品是否已经在购物车中
-        if ($cart = $user->cartItems()->where('product_sku_id', $skuId)->first()) {
-
-            // 如果存在则直接叠加商品数量
-            $cart->update([
-                'amount' => $cart->amount + $amount,
-            ]);
-        } else {
-
-            // 否则创建一个新的购物车记录
-            $cart = new CartItem(['amount' => $amount]);
-            $cart->user()->associate($user);
-            $cart->productSku()->associate($skuId);
-            $cart->save();
-        }
+        $this->cartService->add($request->input('sku_id'), $request->input('amount'));
 
         return [];
     }
@@ -37,14 +26,14 @@ class CartController extends Controller
     public function index(Request $request)
     {
         $addresses=$request->user()->addresses()->orderBy('last_used_at','desc')->get();
-        $cartItems=$request->user()->cartItems()->with(['productSku.product'])->get();
+        $cartItems=$this->cartService->get();
 
         return view('cart.index', ['cartItems' => $cartItems,'addresses'=>$addresses]);
     }
 
     public function remove(ProductSku $sku, Request $request)
     {
-        $request->user()->cartItems()->where('product_sku_id', $sku->id)->delete();
+        $this->cartService->remove($sku->id);
 
         return [];
     }
